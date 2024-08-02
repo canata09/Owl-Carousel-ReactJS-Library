3d59e2d4e66ebbe42f10c95a1f65a926fe777e2b
Can Ata <canata09074@gmail.com>
2024-08-02 16:56:29 +0300

    main
    main

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OwlCarouselComponent from './OwlCarouselComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <OwlCarouselComponent />
+    </div>
+  );
+}
+
+export default App;
